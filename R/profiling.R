11862b2161ceaea00d1e55aeb38c4929a4937570
hankolofs <treffin@gmail.com>
2020-10-08 22:18:27 +0200

    Updated tests and made some profiling trials.

--- R/profiling.R
+++ R/profiling.R
@@ -15,3 +15,90 @@ h <- function(...) {
   pause(0.1)
   greedy_knapsack(knapsack_objects[1:8,],2000)
 }
+
+
+Rprof(tmp <- tempfile(), line.profiling = TRUE)
+compareMethods()
+Rprof()
+summaryRprof()
+unlink(tmp)
+
+lineprof(brute_force_knapsack(knapsack_objects[1:12,],2000), interval = 0.01, torture = FALSE)
+
+# one operation takes a lot of time!
+lineprof(knapsack_dynamic(knapsack_objects[1:20,],3500), interval = 0.01, torture = FALSE)
+
+lineprof(greedy_knapsack(knapsack_objects[1:500,],1000000), interval = 0.01, torture = FALSE)
+
+tmp <- tempfile()
+Rprof(tmp, interval = 0.1)
+knapsack_dynamic_prof(knapsack_objects[1:18,],5000)
+Rprof(NULL)
+
+library(profvis)
+
+
+profvis({knapsack_dynamic_prof(knapsack_objects[1:12,],200)}, interval = 0.01)
+
+profvis({
+  knapsack_dynamic_prof <-
+  function(x, W) {
+    pause(0.1)
+    stopifnot("x is not a data.frame." = is.data.frame(x))
+    stopifnot("data.frame must contain exactly two variables" = ncol(x)==2)
+    stopifnot("data.frame must contain the two variables v and w" = colnames(x) == c("v","w")||colnames(x) == c("w", "v"))
+    stopifnot("v and w must be positive values" = x[,1:2] > 0)
+    pause(0.1)
+    n <- nrow(x)
+    w <- x$w
+    v <- x$v
+    
+    pause(0.1)
+    subset <- matrix(NA, nrow = n+1, ncol = W+1)
+    value <- matrix(NA, nrow = n+1, ncol = W+1)
+    pause(0.1)
+    
+    for (j in 0:W+1) {
+      pause(0.1)
+      value[1,j] <- 0
+      subset[1,j] <- 0
+    }
+    
+    pause(0.1)
+    
+    for (i in 2:(n + 1)) {
+      pause(0.1)
+      for (j in 0:W + 1) {
+        if (w[i - 1] > j) {
+          value[i, j] <- value[i - 1, j]
+          subset[i, j] <- 0
+        } else {
+          pause(0.1)
+          value[i, j] <- max(value[i - 1, j], value[i - 1, j - w[i - 1]] + v[i - 1])
+          if ((w[i - 1] <= j) &
+              all(value[i - 1, j - w[i - 1]] + v[i - 1] > value[i - 1, j])) {
+            pause(0.1)
+            subset[i, j] <- 1
+          } else {
+            pause(0.1)
+            subset[i, j] <- 0
+          }
+        }
+      }
+    }
+    
+    elements <- c()
+    K <- W
+    for(i in (n+1):2) {
+      
+      if(subset[i, K+1] == 1) {
+        pause(0.1)
+        elements <- append(elements,i-1)
+        
+        K <- K-w[i-1]
+      }
+    }
+    pause(0.1)
+    return(list("value" = round(value[nrow(value), ncol(value)]), "elements" = sort(elements)))
+  }
+}, interval = 0.001)
--- tests/testthat/test_knapsack_dynamic.R
+++ tests/testthat/test_knapsack_dynamic.R
@@ -1,12 +1,17 @@
 context("knapsack_dynamic")
 
 suppressWarnings(RNGversion("3.5.9"))
-set.seed(42)
-n <- 2000
-knapsack_objects <- data.frame(
-  w=sample(1:4000, size = n, replace = TRUE),
-  v=runif(n = n, 0, 10000)
-)
+
+data("knapsack_objects")
+# set.seed(42)
+# n <- 2000
+# knapsack_objects <- data.frame(
+#   w=sample(1:4000, size = n, replace = TRUE),
+#   v=runif(n = n, 0, 10000)
+# )
+# 
+# print(knapsack_objects[1:8,])
+
 # Implement test suites
 set.seed(42)
 n <- 2000
@@ -24,7 +29,7 @@ test_that("functions rejects errounous input.", {
 
 test_that("Function return correct results.", {
   kd <- knapsack_dynamic(x = knapsack_objects[1:19,], W = 2445)
-  expect_equal(round(kd$value), 1896544)
+  expect_equal(object = round(kd$value), expected = 18965)
   expect_true(all(round(kd$elements) %in% c(5,7,13,15)))
   
   kd <- knapsack_dynamic(x = knapsack_objects[1:12,], W = 1000)
@@ -35,24 +40,23 @@ test_that("Function return correct results.", {
   expect_true(as.numeric(st)[2] >= 0.00)
 })
 
-
 # Same tests as for brute force
 test_that("Function return correct results.", {
   kd <- knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500)
-  expect_equal(round(kd$value), 16770)
-  expect_true(all(round(kd$elements) %in% c(5, 8)))
+  expect_equal(round(kd$value), 17277)
+  expect_true(all(round(kd$elements) %in% c(4, 5, 7)))
   
   kd <- knapsack_dynamic(x = knapsack_objects[1:12,], W = 3500)
-  expect_equal(round(kd$value), 16770)
-  expect_true(all(round(kd$elements) %in% c(5, 8)))
+  expect_equal(round(kd$value), 19912)
+  expect_true(all(round(kd$elements) %in% c(5, 7, 10)))
   
   kd <- knapsack_dynamic(x = knapsack_objects[1:8,], W = 2000)
-  expect_equal(round(kd$value), 15428)
-  expect_true(all(round(kd$elements) %in% c(3, 8)))
+  expect_equal(round(kd$value), 11498)
+  expect_true(all(round(kd$elements) %in% c(5, 7)))
   
   kd <- knapsack_dynamic(x = knapsack_objects[1:12,], W = 2000)
-  expect_equal(round(kd$value), 15428)
-  expect_true(all(round(kd$elements) %in% c(3, 8)))
+  expect_equal(round(kd$value), 13775)
+  expect_true(all(round(kd$elements) %in% c(7, 10)))
   
   st <- system.time(kd <- knapsack_dynamic(x = knapsack_objects[1:16,], W = 2000))
   expect_true(as.numeric(st)[2] >= 0.00)
